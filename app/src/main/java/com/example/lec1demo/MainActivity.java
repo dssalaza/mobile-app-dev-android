483da0760fb44b470fcb31d2bccf99c0ead60d7f
David Salazar <dssalaza@gmail.com>
2023-01-12 12:24:16 -0800

    Added second activity and linking button

--- app/src/main/java/com/example/lec1demo/MainActivity.java
+++ app/src/main/java/com/example/lec1demo/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.lec1demo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnShowRecipe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //run time view binding
+        btnShowRecipe = findViewById(R.id.btnShowRecipe);
+
+        btnShowRecipe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(
+                        MainActivity.this,
+                        RecipeActivity.class));
+            }
+        });
+
     }
+
+
 }
